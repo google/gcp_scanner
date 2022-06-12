93bd1fe65d6a209a94d107518dd32db9c2fed51a
Maksim Shudrak <mshudrak@google.com>
2022-06-12 20:56:26 +0000

    Adding forgotten acceptance_test

---
+++ test_acceptance.py
@@ -0,0 +1,110 @@
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""The module to perform functional test of the GCP Scanner.
+
+"""
+
+import scanner
+import unittest.mock
+import os
+import json
+
+RESOURCE_COUNT = 26
+RESULTS_JSON_COUNT = 2
+PROJECT_INFO_COUNT = 5
+IAM_POLICY_COUNT = 14
+COMPUTE_INSTANCES_COUNT = 3
+COMPUTE_IMAGES_COUNT = 1
+COMPUTE_DISKS_COUNT = 3
+STATIC_IPS_COUNT = 1
+COMPUTE_SNAPSHOTS_COUNT = 1
+SUBNETS_COUNT = 35
+FIREWALL_RULES_COUNT = 8
+APP_SERVICES_COUNT = 2
+STORAGE_BUCKETS_COUNT = 5
+MANAGED_ZONES_COUNT = 1
+GKE_CLUSTERS_COUNT = 1
+GKE_IMAGES_COUNT = 4
+SQL_INSTANCES_COUNT = 1
+BQ_COUNT = 1
+BIGTABLE_COUNT = 0
+SPANNER_COUNT = 1
+CLOUDSTORE_COUNT = 1
+PUBSUB_COUNT = 1
+CLOUD_FUNCTIONS = 1
+ENDPOINTS_COUNT = 0
+KMS_COUNT = 1
+SERVICES_COUNT = 1
+SERVICE_ACCOUNTS_COUNT = 3
+
+def check_obj_entry(res_dict, subojects_count, entry_name):
+  obj = res_dict.get(entry_name, None)
+  assert obj is not None and len(obj) == subojects_count
+
+def validate_result():
+  with open("res/mshudrak@google.com.json", "r", encoding="utf-8") as f:
+    res_data = json.load(f)
+
+  # project
+  project = res_data["projects"].get("test-gcp-scanner", None)
+  assert project is not None
+  assert len(project) == RESOURCE_COUNT
+
+
+  check_obj_entry(project, PROJECT_INFO_COUNT, "project_info")
+  check_obj_entry(project, IAM_POLICY_COUNT, "iam_policy")
+  check_obj_entry(project, SERVICE_ACCOUNTS_COUNT, "service_accounts")
+
+  check_obj_entry(project, COMPUTE_INSTANCES_COUNT, "compute_instances")
+  check_obj_entry(project, COMPUTE_IMAGES_COUNT, "compute_images")
+  check_obj_entry(project, COMPUTE_DISKS_COUNT, "compute_disks")
+  check_obj_entry(project, COMPUTE_SNAPSHOTS_COUNT, "compute_snapshots")
+
+  check_obj_entry(project, STATIC_IPS_COUNT, "static_ips")
+  check_obj_entry(project, SUBNETS_COUNT, "subnets")
+  check_obj_entry(project, FIREWALL_RULES_COUNT, "firewall_rules")
+  check_obj_entry(project, MANAGED_ZONES_COUNT, "managed_zones")
+
+  check_obj_entry(project, APP_SERVICES_COUNT, "app_services")
+
+  check_obj_entry(project, STORAGE_BUCKETS_COUNT, "storage_buckets")
+
+  check_obj_entry(project, GKE_CLUSTERS_COUNT, "gke_clusters")
+  check_obj_entry(project, GKE_IMAGES_COUNT, "gke_images")
+
+  check_obj_entry(project, SQL_INSTANCES_COUNT, "sql_instances")
+  check_obj_entry(project, BQ_COUNT, "bq")
+  check_obj_entry(project, BIGTABLE_COUNT, "bigtable_instances")
+  check_obj_entry(project, SPANNER_COUNT, "spanner_instances")
+  check_obj_entry(project, CLOUDSTORE_COUNT, "cloudstore_instances")
+
+  check_obj_entry(project, PUBSUB_COUNT, "pubsub_subs")
+  check_obj_entry(project, CLOUD_FUNCTIONS, "cloud_functions")
+  check_obj_entry(project, ENDPOINTS_COUNT, "endpoints")
+
+  check_obj_entry(project, KMS_COUNT, "kms")
+
+  check_obj_entry(project, SERVICES_COUNT, "services")
+
+
+def test_acceptance():
+  os.mkdir("res")
+  testargs = ["__main__.py", "-m", "-p", "test-gcp-scanner", "-o", "res"]
+  with unittest.mock.patch("sys.argv", testargs):
+    assert scanner.main() == 0
+    assert len(os.listdir("res/")) == RESULTS_JSON_COUNT # TODO: Fix it
+    validate_result()
+
+
