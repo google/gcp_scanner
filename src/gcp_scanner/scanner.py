62832bf8e6e435225b6e2928bbf53e48bc516067
James Martini <martinij29@gmail.com>
2023-02-10 19:47:47 -0500

    Corrected the models package context and deleted duplicate argparser import

--- src/gcp_scanner/scanner.py
+++ src/gcp_scanner/scanner.py
@@ -31,8 +31,7 @@ from google.cloud import iam_credentials
 from google.cloud.iam_credentials_v1.services.iam_credentials.client import IAMCredentialsClient
 from googleapiclient import discovery
 from httplib2 import Credentials
-from models import SpiderContext
-import argparser
+from .models import SpiderContext
 
 
 def is_set(config, config_setting):
