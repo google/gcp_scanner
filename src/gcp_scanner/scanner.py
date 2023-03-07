98c13a842e4bb7543d0c825e837494cae80ae7e3
Abdelrahman mostafa <a.abdo.mae@gmail.com>
2023-03-07 18:51:44 +0200

    added the ability to disable all logs

--- src/gcp_scanner/scanner.py
+++ src/gcp_scanner/scanner.py
@@ -393,11 +393,13 @@ token_uri and client_secret stored in JSON format.'
   force_projects_list = list()
   if args.force_projects:
     force_projects_list = args.force_projects.split(',')
-
-  if args.log_level == 'DISABLED':
+  if args.log_level != 'DISABLED':
      logging_level = getattr(logging, args.log_level.upper(), None) or logging.INFO
+  else:
+    logging = None
   logging.basicConfig(level=logging_level,
-                      format="%(asctime)s - %(levelname)s - %(message)s",datefmt="%d-%b-%y %H:%M:%S")
+                      format="%(asctime)s - %(levelname)s - %(message)s",
+                      datefmt="%d-%b-%y %H:%M:%S")
 
   sa_tuples = []
   if args.key_path:
