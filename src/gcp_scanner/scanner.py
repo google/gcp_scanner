dd556273babec37b4ca348675015c0e2568cb388
Abdelrahman mostafa <a.abdo.mae@gmail.com>
2023-03-08 13:00:13 +0200

    looging formate

--- src/gcp_scanner/scanner.py
+++ src/gcp_scanner/scanner.py
@@ -394,7 +394,8 @@ token_uri and client_secret stored in JSON format.'
   if args.force_projects:
     force_projects_list = args.force_projects.split(',')
 
-  logging.basicConfig(level=getattr(logging, args.log_level.upper(), None))
+  logging.basicConfig(level=getattr(logging, args.log_level.upper(), None),
+                      format='%(asctime)s - %(levelname)s - %(message)s',)
 
   sa_tuples = []
   if args.key_path:
