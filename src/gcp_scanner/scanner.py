6b2f1e5b9dbc9cc1c0ad8f09856f324d3d319ea8
Abdelrahman mostafa <a.abdo.mae@gmail.com>
2023-03-08 20:14:38 +0200

    debugging pytest error

--- src/gcp_scanner/scanner.py
+++ src/gcp_scanner/scanner.py
@@ -394,9 +394,7 @@ token_uri and client_secret stored in JSON format.'
   if args.force_projects:
     force_projects_list = args.force_projects.split(',')
 
-  logging.basicConfig(level=getattr(logging, args.log_level.upper(), None),
-                      format='%(asctime)s - %(levelname)s - %(message)s',
-                      datefmt='%Y-%m-%d %H:%M:%S')
+  logging.basicConfig(level=getattr(logging, args.log_level.upper(), None))
 
   sa_tuples = []
   if args.key_path:
