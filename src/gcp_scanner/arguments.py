3baf024c561a81273728c0a73596e2363a7c4946
Abdelrahman mostafa <a.abdo.mae@gmail.com>
2023-03-10 23:25:08 +0200

    Move arguments parser into individual function which closes #15

---
+++ src/gcp_scanner/arguments.py
@@ -0,0 +1,122 @@
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+"""Command-Line Argument Parser
+
+"""
+
+import argparse
+import logging
+
+def parser():
+  parser = argparse.ArgumentParser(
+      prog='scanner.py',
+      description='GCP Scanner',
+      usage='python3 %(prog)s -o folder_to_save_results -g -')
+  required_named = parser.add_argument_group('Required parameters')
+  required_named.add_argument(
+      '-o',
+      '--output-dir',
+      required=True,
+      dest='output',
+      default='scan_db',
+      help='Path to output directory')
+
+  parser.add_argument(
+      '-k',
+      '--sa-key-path',
+      default=None,
+      dest='key_path',
+      help='Path to directory with SA keys in json format')
+  parser.add_argument(
+      '-g',
+      '--gcloud-profile-path',
+      default=None,
+      dest='gcloud_profile_path',
+      help='Path to directory with gcloud profile. Specify -\
+ to search for credentials in default gcloud config path'
+  )
+  parser.add_argument(
+      '-m',
+      '--use-metadata',
+      default=False,
+      dest='use_metadata',
+      action='store_true',
+      help='Extract credentials from GCE instance metadata')
+  parser.add_argument(
+      '-at',
+      '--access-token-files',
+      default=None,
+      dest='access_token_files',
+      help='A list of comma separated files with access token and OAuth scopes.\
+TTL limited. A token and scopes should be stored in JSON format.')
+  parser.add_argument(
+      '-rt',
+      '--refresh-token-files',
+      default=None,
+      dest='refresh_token_files',
+      help='A list of comma separated files with refresh_token, client_id,\
+token_uri and client_secret stored in JSON format.'
+  )
+  parser.add_argument(
+      '-s',
+      '--service-account',
+      default=None,
+      dest='key_name',
+      help='Name of individual SA to scan')
+  parser.add_argument(
+      '-p',
+      '--project',
+      default=None,
+      dest='target_project',
+      help='Name of individual project to scan')
+  parser.add_argument(
+      '-f',
+      '--force-projects',
+      default=None,
+      dest='force_projects',
+      help='Comma separated list of project names to include in the scan')
+  parser.add_argument(
+      '-c',
+      '--config',
+      default=None,
+      dest='config_path',
+      help='A path to config file with a set of specific resources to scan.')
+  parser.add_argument(
+      '-l',
+      '--logging',
+      default='WARNING',
+      dest='log_level',
+      choices=('INFO', 'WARNING', 'ERROR'),
+      help='Set logging level (INFO, WARNING, ERROR)')
+  parser.add_argument(
+      '-lf',
+      '--log-file',
+      default=False,
+      dest='log_file',
+      action='store_true',
+      help='Save logs to logs.log file rather than displaying in console')
+
+  args: argparse.Namespace = parser.parse_args()
+
+  if not args.key_path and not args.gcloud_profile_path \
+    and not args.use_metadata and not args.access_token_files\
+    and not args.refresh_token_files:
+    logging.error(
+        'Please select at least one option to begin scan\
+ -k/--sa-key-path,-g/--gcloud-profile-path, -m, -rt, -at'
+    )
+
+  return args
--- src/gcp_scanner/scanner.py
+++ src/gcp_scanner/scanner.py
@@ -17,7 +17,6 @@
 
 """
 
-import argparse
 import json
 import logging
 import os
@@ -26,6 +25,7 @@ from typing import List, Tuple, Dict, Optional,Union
 
 from . import crawl
 from . import credsdb
+from . import arguments
 from google.cloud import container_v1
 from google.cloud import iam_credentials
 from google.cloud.iam_credentials_v1.services.iam_credentials.client import IAMCredentialsClient
@@ -299,109 +299,13 @@ def main():
   logging.getLogger('googleapiclient.discovery_cache').setLevel(logging.ERROR)
   logging.getLogger('googleapiclient.http').setLevel(logging.ERROR)
 
-  parser = argparse.ArgumentParser(
-      prog='scanner.py',
-      description='GCP Scanner',
-      usage='python3 %(prog)s -o folder_to_save_results -g -')
-  required_named = parser.add_argument_group('Required parameters')
-  required_named.add_argument(
-      '-o',
-      '--output-dir',
-      required=True,
-      dest='output',
-      default='scan_db',
-      help='Path to output directory')
-
-  parser.add_argument(
-      '-k',
-      '--sa-key-path',
-      default=None,
-      dest='key_path',
-      help='Path to directory with SA keys in json format')
-  parser.add_argument(
-      '-g',
-      '--gcloud-profile-path',
-      default=None,
-      dest='gcloud_profile_path',
-      help='Path to directory with gcloud profile. Specify -\
- to search for credentials in default gcloud config path'
-  )
-  parser.add_argument(
-      '-m',
-      '--use-metadata',
-      default=False,
-      dest='use_metadata',
-      action='store_true',
-      help='Extract credentials from GCE instance metadata')
-  parser.add_argument(
-      '-at',
-      '--access-token-files',
-      default=None,
-      dest='access_token_files',
-      help='A list of comma separated files with access token and OAuth scopes.\
-TTL limited. A token and scopes should be stored in JSON format.')
-  parser.add_argument(
-      '-rt',
-      '--refresh-token-files',
-      default=None,
-      dest='refresh_token_files',
-      help='A list of comma separated files with refresh_token, client_id,\
-token_uri and client_secret stored in JSON format.'
-  )
-  parser.add_argument(
-      '-s',
-      '--service-account',
-      default=None,
-      dest='key_name',
-      help='Name of individual SA to scan')
-  parser.add_argument(
-      '-p',
-      '--project',
-      default=None,
-      dest='target_project',
-      help='Name of individual project to scan')
-  parser.add_argument(
-      '-f',
-      '--force-projects',
-      default=None,
-      dest='force_projects',
-      help='Comma separated list of project names to include in the scan')
-  parser.add_argument(
-      '-c',
-      '--config',
-      default=None,
-      dest='config_path',
-      help='A path to config file with a set of specific resources to scan.')
-  parser.add_argument(
-      '-l',
-      '--logging',
-      default='WARNING',
-      dest='log_level',
-      choices=('INFO', 'WARNING', 'ERROR'),
-      help='Set logging level (INFO, WARNING, ERROR)')
-  parser.add_argument(
-      '-lf',
-      '--log-file',
-      default=False,
-      dest='log_file',
-      action='store_true',
-      help='Save logs to logs.log file rather than displaying in console')
-
-  args: argparse.Namespace = parser.parse_args()
-
-  if not args.key_path and not args.gcloud_profile_path \
-    and not args.use_metadata and not args.access_token_files\
-    and not args.refresh_token_files:
-    logging.error(
-        'Please select at least one option to begin scan\
- -k/--sa-key-path,-g/--gcloud-profile-path, -m, -rt, -at'
-    )
+  args = arguments.parser()
 
   force_projects_list = list()
   if args.force_projects:
     force_projects_list = args.force_projects.split(',')
 
-  if argparse.log_file:
+  if args.log_file:
     logging.basicConfig(filename='../logs.log',
                         level=getattr(logging, args.log_level.upper(), None),
                         format='%(asctime)s - %(levelname)s - %(message)s',
