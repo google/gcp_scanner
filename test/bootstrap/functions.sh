765bae6dcc03fdeb965447b1371881925973b781
Maksim Shudrak <mshudrak@google.com>
2023-04-27 11:11:34 -0700

    update cloud_functions

--- test/bootstrap/functions.sh
+++ test/bootstrap/functions.sh
@@ -8,7 +8,8 @@ gcloud functions deploy $FUNCTION_NAME \
   --region $REGION \
   --runtime python310 \
   --entry-point hello_world \
-  --source test-function-1/
+  --source test-function-1/ \
   --max-instances 30 \
   --memory 128MB \
-  --ingress-settings ALL
+  --ingress-settings ALL \
+  --trigger-http
