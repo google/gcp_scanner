96c274c306778fa225e81f65d00ecd507d8ad53c
Sudipto Baral <sudiptobaral.me@gmail.com>
2023-05-28 23:20:26 +0600

    refactor:hammer:: update unit test for get_sql_instances with new method signature.

--- src/gcp_scanner/test_unit.py
+++ src/gcp_scanner/test_unit.py
@@ -424,7 +424,10 @@ class TestCrawler(unittest.TestCase):
     """Test SQL instances."""
     self.assertTrue(
       verify(
-        crawl.get_sql_instances(PROJECT_NAME, self.credentials),
+        crawl.get_sql_instances(
+          PROJECT_NAME,
+          ClientFactory.get_client("sqladmin").get_service(self.credentials),
+        ),
         "sql_instances",
         True,
       )
