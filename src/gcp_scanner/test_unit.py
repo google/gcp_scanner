fee1e4e6a91d32dc2154601604456d7685d0677e
Sudipto Baral <sudiptobaral.me@gmail.com>
2023-05-31 23:55:59 +0600

    test: :rotating_light: : add unit test for appengine client factory.

--- src/gcp_scanner/test_unit.py
+++ src/gcp_scanner/test_unit.py
@@ -33,6 +33,7 @@ from google.oauth2 import credentials
 from . import crawl
 from . import credsdb
 from . import scanner
+from .client.appengine_client import AppEngineClient
 from .client.client_factory import ClientFactory
 from .client.compute_client import ComputeClient
 from .client.dns_client import DNSClient
@@ -575,6 +576,11 @@ class TestClientFactory(unittest.TestCase):
     """Test get_client method with 'compute' name."""
     client = ClientFactory.get_client("compute")
     self.assertIsInstance(client, ComputeClient)
+
+  def test_get_client_appengine(self):
+    """Test get_client method with 'appengine' name."""
+    client = ClientFactory.get_client("appengine")
+    self.assertIsInstance(client, AppEngineClient)
 
   def test_get_client_invalid(self):
     """Test get_client method with invalid name."""
