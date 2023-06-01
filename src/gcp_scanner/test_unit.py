df8ac9d67e866291499766b0a67e0b24e0a999bf
Sudipto Baral <sudiptobaral.me@gmail.com>
2023-06-01 21:47:25 +0600

    test::rotating_light:: add unit test for filestore client factory.

--- src/gcp_scanner/test_unit.py
+++ src/gcp_scanner/test_unit.py
@@ -40,6 +40,7 @@ from .client.client_factory import ClientFactory
 from .client.cloud_functions_client import CloudFunctionsClient
 from .client.compute_client import ComputeClient
 from .client.dns_client import DNSClient
+from .client.filestore_client import FilestoreClient
 from .client.pubsub_client import PubSubClient
 from .client.spanner_client import SpannerClient
 from .client.sql_client import SQLClient
@@ -512,7 +513,10 @@ class TestCrawler(unittest.TestCase):
     """Test FileStore Instances."""
     self.assertTrue(
       verify(
-        crawl.get_filestore_instances(PROJECT_NAME, self.credentials),
+        crawl.get_filestore_instances(
+          PROJECT_NAME,
+          ClientFactory.get_client("file").get_service(self.credentials),
+        ),
         "filestore_instances",
       )
     )
@@ -647,6 +651,11 @@ class TestClientFactory(unittest.TestCase):
     """Test get_client method with 'spanner' name."""
     client = ClientFactory.get_client("spanner")
     self.assertIsInstance(client, SpannerClient)
+
+  def test_get_client_filestore(self):
+    """Test get_client method with 'spanner' name."""
+    client = ClientFactory.get_client("file")
+    self.assertIsInstance(client, FilestoreClient)
 
   def test_get_client_invalid(self):
     """Test get_client method with invalid name."""
