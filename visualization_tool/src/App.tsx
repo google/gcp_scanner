d3cd2f690f8b6f551886f247ebd54bc534bf9599
Yahia Zakaria <yahiazakaria3200@gmail.com>
2023-06-01 22:50:47 +0300

    feat :tada: :Intial commit for visualization tool
    Add init files for react setup with vite and TS

---
+++ visualization_tool/src/App.tsx
@@ -0,0 +1,18 @@
+import viteLogo from '/logo.png'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+      </div>
+      <h1>Hello GCP Scanner!</h1>
+    </>
+  )
+}
+
+export default App
