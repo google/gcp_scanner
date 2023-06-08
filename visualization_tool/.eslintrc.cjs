4a629fc3969c5cf9853bc14f783eee3139ecce92
Yahia Zakaria <yahiazakaria3200@gmail.com>
2023-06-08 17:20:39 +0300

    style :art: Adding linting and formating
    Configure eslint and prettier

--- visualization_tool/.eslintrc.cjs
+++ visualization_tool/.eslintrc.cjs
@@ -1,14 +1,32 @@
 module.exports = {
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
   },
-}
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint", "react"],
+  rules: {
+    "@typescript-eslint/naming-convention": [
+        "error",
+        {
+            "selector": ["variable", "function", "parameter"],
+            "format": ["camelCase", "PascalCase"],
+        },
+        {
+            "selector": "typeLike",
+            "format": ["PascalCase"],
+        }
+    ],
+  },
+};
--- visualization_tool/src/App.tsx
+++ visualization_tool/src/App.tsx
@@ -1,18 +1,21 @@
-import logo from '/logo.png'
-import './App.css'
+import logo from '/logo.png';
+import './App.css';
 
 function App() {
-
   return (
     <>
       <div>
-        <a href="https://github.com/google/gcp_scanner" target="_blank">
-          <img src={logo} className="logo" alt="Vite logo" />
+        <a
+          href="https://github.com/google/gcp_scanner"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={logo} className="logo" alt="GCP Scanner logo" />
         </a>
       </div>
       <h1>Hello GCP Scanner!</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
