22a7c3d04fd8dec8580e815ef2abb79e633fea50
peb <singh.harsh9097@gmail.com>
2023-06-12 23:13:02 +0530

    reactor 🎨: service crawler map & fix: imports

--- src/gcp_scanner/crawler/crawler_factory.py
+++ src/gcp_scanner/crawler/crawler_factory.py
@@ -11,18 +11,31 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
+
 import logging
 
-from gcp_scanner.crawler.app_services_crawler import AppServicesCrawler
-from gcp_scanner.crawler.compute_disks_crawler import ComputeDisksCrawler
-from gcp_scanner.crawler.compute_firewall_rules_crawler import ComputeFirewallRulesCrawler
-from gcp_scanner.crawler.compute_images_crawler import ComputeImagesCrawler
-from gcp_scanner.crawler.compute_instances_crawler import ComputeInstancesCrawler
-from gcp_scanner.crawler.compute_snapshots_crawler import ComputeSnapshotsCrawler
-from gcp_scanner.crawler.compute_static_ips_crawler import ComputeStaticIPsCrawler
-from gcp_scanner.crawler.compute_subnets_crawler import ComputeSubnetsCrawler
-from gcp_scanner.crawler.machine_images_crawler import ComputeMachineImagesCrawler
+from src.gcp_scanner.crawler.app_services_crawler import AppServicesCrawler
+from src.gcp_scanner.crawler.compute_disks_crawler import ComputeDisksCrawler
+from src.gcp_scanner.crawler.compute_firewall_rules_crawler import ComputeFirewallRulesCrawler
+from src.gcp_scanner.crawler.compute_images_crawler import ComputeImagesCrawler
+from src.gcp_scanner.crawler.compute_instances_crawler import ComputeInstancesCrawler
+from src.gcp_scanner.crawler.compute_snapshots_crawler import ComputeSnapshotsCrawler
+from src.gcp_scanner.crawler.compute_static_ips_crawler import ComputeStaticIPsCrawler
+from src.gcp_scanner.crawler.compute_subnets_crawler import ComputeSubnetsCrawler
+from src.gcp_scanner.crawler.machine_images_crawler import ComputeMachineImagesCrawler
 
+
+service_crawler_map = {
+  "app_services": AppServicesCrawler(),
+  "compute_disks": ComputeDisksCrawler(),
+  "compute_images": ComputeImagesCrawler(),
+  "compute_instances": ComputeInstancesCrawler(),
+  "compute_snapshots": ComputeSnapshotsCrawler(),
+  "firewall_rules": ComputeFirewallRulesCrawler(),
+  "machine_images": ComputeMachineImagesCrawler(),
+  "static_ips": ComputeStaticIPsCrawler(),
+  "subnets": ComputeSubnetsCrawler(),
+}
 
 class CrawlerFactory:
   """Factory class for creating crawlers."""
@@ -30,24 +43,8 @@ class CrawlerFactory:
   @classmethod
   def create_crawler(cls, name):
     """Returns the appropriate crawler."""
-    if name == "app_services":
-      return AppServicesCrawler()
-    if name == "compute_disks":
-      return ComputeDisksCrawler()
-    if name == "firewall_rules":
-      return ComputeFirewallRulesCrawler()
-    if name == "compute_images":
-      return ComputeImagesCrawler()
-    if name == "compute_instances":
-      return ComputeInstancesCrawler()
-    if name == "machine_images":
-      return ComputeMachineImagesCrawler()
-    if name == "compute_snapshots":
-      return ComputeSnapshotsCrawler()
-    if name == "static_ips":
-      return ComputeStaticIPsCrawler()
-    if name == "subnets":
-      return ComputeSubnetsCrawler()
+    if name in service_crawler_map:
+      return service_crawler_map[name]
 
     logging.error("Crawler not supported.")
     return None
