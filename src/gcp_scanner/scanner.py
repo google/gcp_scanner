bb26aa4f9cc6d8df22493d3359c1880b36cf8c05
peb <singh.harsh9097@gmail.com>
2023-06-22 02:00:13 +0530

    refactor: modify scan loop

--- src/gcp_scanner/scanner.py
+++ src/gcp_scanner/scanner.py
@@ -383,7 +383,9 @@ def crawl_loop(initial_sa_tuples: List[Tuple[str, Credentials, List[str]]],
 
       # Get list of KMS keys
       if is_set(scan_config, 'kms'):
-        project_result['kms'] = crawl.get_kms_keys(
+        project_result['kms'] = CrawlerFactory.create_crawler(
+          'kms',
+        ).crawl(
           project_id,
           ClientFactory.get_client('cloudkms').get_service(credentials),
         )
