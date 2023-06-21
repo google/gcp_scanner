10e6ac3b5fecf58f5e1576875d012bd0b1dd4859
Harsh <77539004+peb-peb@users.noreply.github.com>
2023-06-22 04:05:50 +0530

    refactor: Crawler/bq (#222)
    * refactor: create `BigQueryCrawler` and helper func
    * refactor: arrange map alphabetically

--- src/gcp_scanner/crawler/crawler_factory.py
+++ src/gcp_scanner/crawler/crawler_factory.py
@@ -15,6 +15,7 @@
 import logging
 
 from gcp_scanner.crawler.app_services_crawler import AppServicesCrawler
+from gcp_scanner.crawler.bigquery_crawler import BigQueryCrawler
 from gcp_scanner.crawler.bigtable_instances_crawler import BigTableInstancesCrawler
 from gcp_scanner.crawler.cloud_functions_crawler import CloudFunctionsCrawler
 from gcp_scanner.crawler.compute_disks_crawler import ComputeDisksCrawler
@@ -37,24 +38,25 @@ from gcp_scanner.crawler.service_usage_crawler import ServiceUsageCrawler
 
 service_crawler_map = {
   "app_services": AppServicesCrawler,
-  "cloud_functions": CloudFunctionsCrawler,
   "bigtable_instances": BigTableInstancesCrawler,
+  "bq": BigQueryCrawler,
+  "cloud_functions": CloudFunctionsCrawler,
   "compute_disks": ComputeDisksCrawler,
   "compute_images": ComputeImagesCrawler,
   "compute_instances": ComputeInstancesCrawler,
   "compute_snapshots": ComputeSnapshotsCrawler,
-  "filestore_instances": FilestoreInstancesCrawler,
   "dns_policies": DNSPoliciesCrawler,
+  "filestore_instances": FilestoreInstancesCrawler,
   "firewall_rules": ComputeFirewallRulesCrawler,
   "machine_images": ComputeMachineImagesCrawler,
-  "sql_instances": SQLInstancesCrawler,
-  "spanner_instances": SpannerInstancesCrawler,
-  "pubsub_subs": PubSubSubscriptionsCrawler,
   "managed_zones": DNSManagedZonesCrawler,
+  "pubsub_subs": PubSubSubscriptionsCrawler,
   "services": ServiceUsageCrawler,
+  "sourcerepos": CloudSourceRepoCrawler,
+  "spanner_instances": SpannerInstancesCrawler,
+  "sql_instances": SQLInstancesCrawler,
   "static_ips": ComputeStaticIPsCrawler,
   "subnets": ComputeSubnetsCrawler,
-  "sourcerepos": CloudSourceRepoCrawler,
 }
 
 
