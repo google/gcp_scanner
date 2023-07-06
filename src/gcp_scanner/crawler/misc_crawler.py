f1581af0ce926b6709c5dd61b094cc540fed23dd
Sudipto Baral <sudiptobaral.me@gmail.com>
2023-07-06 10:25:04 +0600

    :art: add docstring to the module.

--- src/gcp_scanner/crawler/misc_crawler.py
+++ src/gcp_scanner/crawler/misc_crawler.py
@@ -11,6 +11,9 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+"""
+This module provides miscellaneous crawler methods to crawl GCP resources using REST API
+"""
 import logging
 import sys
 from typing import List, Dict, Any, Tuple
