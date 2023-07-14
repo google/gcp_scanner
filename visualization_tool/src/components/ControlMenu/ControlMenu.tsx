ee013ef373f3bcdb26d1296dfb5b47212724133d
Yahia Zakaria <yahiazakaria3200@gmail.com>
2023-07-14 19:42:41 +0300

    add errors handling

--- visualization_tool/src/components/ControlMenu/ControlMenu.tsx
+++ visualization_tool/src/components/ControlMenu/ControlMenu.tsx
@@ -1,9 +1,9 @@
 import {useState, useRef} from 'react';
 import AddIcon from '@mui/icons-material/Add';
 import CloseIcon from '@mui/icons-material/Close';
+// validate schema with joi
 
 import {Resource} from '../../types/resources';
-import {OutputFile} from '../../types/resources';
 
 import {parseData} from './Controller';
 
@@ -16,6 +16,8 @@ type ControlMenuProps = {
 const ControlMenu = ({setResources}: ControlMenuProps) => {
   const fileInput = useRef<HTMLInputElement>(null);
   const [files, setFiles] = useState<string[]>([]);
+  const [error, setError] = useState<string | null>(null);
+
   return (
     <div className="control-menu">
       <div className="menu-item">
@@ -29,24 +31,36 @@ const ControlMenu = ({setResources}: ControlMenuProps) => {
           className="upload-form"
           onSubmit={e => {
             e.preventDefault();
+            setError(null);
             const file = fileInput.current?.files?.[0];
 
-            if (!file) return;
+            if (!file) {
+              setError('No file selected');
+              return;
+            }
             // check if file is already uploaded
-            if (files.includes(file.name)) return;
+            if (files.includes(file.name)) {
+              setError('File already uploaded');
+              return;
+            }
+
             const reader = new FileReader();
             reader.readAsText(file);
             reader.onload = e => {
               const result = e.target?.result as string;
-              const data = JSON.parse(result) as OutputFile;
 
-              const resources = parseData(data, file.name);
-
-              setResources((prevResources: Resource[]) => [
-                ...prevResources,
-                ...resources,
-              ]);
-              setFiles([...files, file.name]);
+              try {
+                const data = JSON.parse(result);
+                const resources = parseData(data, file.name);
+                setResources((prevResources: Resource[]) => [
+                  ...prevResources,
+                  ...resources,
+                ]);
+                setFiles([...files, file.name]);
+              } catch (err) {
+                setError('Invalid file');
+                return;
+              }
             };
           }}
         >
@@ -67,6 +81,7 @@ const ControlMenu = ({setResources}: ControlMenuProps) => {
             />
           </button>
         </form>
+        {error && <p className="error">{error}</p>}
         {files.length > 0 && (
           <div className="files-list">
             {files.map(file => (
