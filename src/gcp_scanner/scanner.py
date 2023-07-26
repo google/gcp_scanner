f60a8b9d7caa5031adb04e4162f2d5f72f115d53
Harsh <singh.harsh9097@gmail.com>
2023-07-27 04:27:31 +0530

    fix: pylint
    removed unused imports

--- src/gcp_scanner/scanner.py
+++ src/gcp_scanner/scanner.py
@@ -30,7 +30,6 @@ from google.auth.exceptions import MalformedError
 from google.cloud import container_v1
 from google.cloud import iam_credentials
 from google.cloud.iam_credentials_v1.services.iam_credentials.client import IAMCredentialsClient
-from googleapiclient import discovery
 from httplib2 import Credentials
 
 from . import arguments
