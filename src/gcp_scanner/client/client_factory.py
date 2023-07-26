1cf9f0c19eb017a9ab31d090c7ba984052a20ad1
Sudipto Baral <sudiptobaral.me@gmail.com>
2023-07-26 10:57:13 +0600

    :recycle: updated client factory to make it more readable.

--- src/gcp_scanner/client/client_factory.py
+++ src/gcp_scanner/client/client_factory.py
@@ -20,9 +20,9 @@ from gcp_scanner.client.bigtable_client import BigTableClient
 from gcp_scanner.client.cloud_functions_client import CloudFunctionsClient
 from gcp_scanner.client.cloud_resource_manager_client import CloudResourceManagerClient
 from gcp_scanner.client.compute_client import ComputeClient
-from gcp_scanner.client.firestore_client import FirestoreClient
 from gcp_scanner.client.dns_client import DNSClient
 from gcp_scanner.client.filestore_client import FilestoreClient
+from gcp_scanner.client.firestore_client import FirestoreClient
 from gcp_scanner.client.iam_client import IAMClient
 from gcp_scanner.client.kms_client import CloudKMSClient
 from gcp_scanner.client.pubsub_client import PubSubClient
@@ -37,46 +37,34 @@ from gcp_scanner.client.storage_client import StorageClient
 class ClientFactory:
   """Factory class for creating clients."""
 
+  clients = {
+    "appengine": AppEngineClient,
+    "bigquery": BQClient,
+    "bigtableadmin": BigTableClient,
+    "cloudfunctions": CloudFunctionsClient,
+    "cloudkms": CloudKMSClient,
+    "cloudresourcemanager": CloudResourceManagerClient,
+    "compute": ComputeClient,
+    "dns": DNSClient,
+    "firestore": FirestoreClient,
+    "file": FilestoreClient,
+    "iam": IAMClient,
+    "pubsub": PubSubClient,
+    "servicemanagement": ServiceManagementClient,
+    "serviceusage": ServiceUsageClient,
+    "sourcerepo": SourceRepoClient,
+    "spanner": SpannerClient,
+    "sqladmin": SQLClient,
+    "storage": StorageClient,
+  }
+
   @classmethod
   def get_client(cls, name):
     """Returns the appropriate client."""
 
-    if name == "appengine":
-      return AppEngineClient()
-    if name == "bigquery":
-      return BQClient()
-    if name == "bigtableadmin":
-      return BigTableClient()
-    if name == "cloudfunctions":
-      return CloudFunctionsClient()
-    if name == "cloudkms":
-      return CloudKMSClient()
-    if name == "cloudresourcemanager":
-      return CloudResourceManagerClient()
-    if name == "compute":
-      return ComputeClient()
-    if name == "dns":
-      return DNSClient()
-    if name == "firestore":
-      return FirestoreClient()
-    if name == "file":
-      return FilestoreClient()
-    if name == "iam":
-      return IAMClient()
-    if name == "pubsub":
-      return PubSubClient()
-    if name == "servicemanagement":
-      return ServiceManagementClient()
-    if name == "serviceusage":
-      return ServiceUsageClient()
-    if name == "sourcerepo":
-      return SourceRepoClient()
-    if name == "spanner":
-      return SpannerClient()
-    if name == "sqladmin":
-      return SQLClient()
-    if name == "storage":
-      return StorageClient()
+    client_cls = cls.clients.get(name.lower())
+    if client_cls:
+      return client_cls()
 
     logging.error("Client not supported.")
     return None
