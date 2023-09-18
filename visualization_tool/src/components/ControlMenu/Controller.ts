f5f6176f4ef76f074d0bcd4075fcd269312ffbc5
Yahia <yahiazakaria3200@gmail.com>
2023-09-18 18:33:28 +0300

    fix: Handle new output file format (#285)

--- visualization_tool/src/components/ControlMenu/Controller.ts
+++ visualization_tool/src/components/ControlMenu/Controller.ts
@@ -17,47 +17,48 @@ const titleCase = (str: string) => {
 
 const parseData = (data: OutputFile, fileName: string) => {
   const resources = [];
-  for (const [projectId, projectData] of Object.entries(data.projects)) {
-    for (const [resourceType, resourceList] of Object.entries(projectData)) {
-      let type = titleCase(resourceType.slice(0, -1));
-      if (type === 'Dns Policie') type = 'Dns Policy';
-      if (
-        resourceList instanceof Array &&
-        availableResourceTypes.includes(type as ResourceType)
-      ) {
-        const currentResources = [];
-        for (const resource of resourceList) {
-          const resourceData: Record<string, string | number> = {};
-          resourceData['file'] = fileName;
-          resourceData['projectId'] = projectId;
+  const projectId = data.project_info.projectId;
+  console.log(Object.entries(data));
+  for (const [resourceType, resourceList] of Object.entries(data)) {
+    console.log(resourceType);
+    let type = titleCase(resourceType.slice(0, -1));
+    if (type === 'Dns Policie') type = 'Dns Policy';
+    if (
+      resourceList instanceof Array &&
+      availableResourceTypes.includes(type as ResourceType)
+    ) {
+      const currentResources = [];
+      for (const resource of resourceList) {
+        const resourceData: Record<string, string | number> = {};
+        resourceData['file'] = fileName;
+        resourceData['projectId'] = projectId;
 
-          for (const [key, value] of Object.entries(resource)) {
-            switch (typeof value) {
-              case 'string':
-                // if this attribute is a link, get the last part of the link
-                if (value.split('/').length > 1) {
-                  resourceData[key] = value.split('/').at(-1) || 'unknown';
-                } else {
-                  resourceData[key] = value;
-                }
-                break;
-              case 'number':
+        for (const [key, value] of Object.entries(resource)) {
+          switch (typeof value) {
+            case 'string':
+              // if this attribute is a link, get the last part of the link
+              if (value.split('/').length > 1) {
+                resourceData[key] = value.split('/').at(-1) || 'unknown';
+              } else {
                 resourceData[key] = value;
-                break;
-              default:
-                break;
-            }
+              }
+              break;
+            case 'number':
+              resourceData[key] = value;
+              break;
+            default:
+              break;
           }
-
-          resourceData['name'] = resourceData['name'] || 'unknown';
-          resourceData['status'] = resourceData['status'] || 'READY';
-          resourceData['type'] = type;
-
-          currentResources.push(resourceData as Resource);
         }
 
-        resources.push(...currentResources);
+        resourceData['name'] = resourceData['name'] || 'unknown';
+        resourceData['status'] = resourceData['status'] || 'READY';
+        resourceData['type'] = type;
+
+        currentResources.push(resourceData as Resource);
       }
+
+      resources.push(...currentResources);
     }
   }
 
@@ -66,27 +67,25 @@ const parseData = (data: OutputFile, fileName: string) => {
 
 const parseIAMData = (data: OutputFile, fileName: string) => {
   const roles: IAMRole[] = [];
-  for (const [projectId, projectData] of Object.entries(data.projects)) {
-    const currentRoles = projectData?.iam_policy as IMAPolicyField[];
+  const projectId = data.project_info.projectId;
+  const currentRoles = data.iam_policy as IMAPolicyField[];
 
-    if (roles instanceof Array) {
-      for (const role of currentRoles) {
-        roles.push({
-          file: fileName,
-          projectId,
-          role: `${projectId}__${role.role.split('/')[1]}`,
-          members: role.members.map(member => {
-            return {
-              memberType: member.split(':')[0],
-              email: member.split(':')[1],
-            };
-          }),
-        });
-      }
+  if (roles instanceof Array) {
+    for (const role of currentRoles) {
+      roles.push({
+        file: fileName,
+        projectId,
+        role: `${projectId}__${role.role.split('/')[1]}`,
+        members: role.members.map(member => {
+          return {
+            memberType: member.split(':')[0],
+            email: member.split(':')[1],
+          };
+        }),
+      });
     }
   }
 
-  // console.log(roles);
   return roles;
 };
 export {parseData, parseIAMData};
