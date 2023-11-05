181533ca3a7601f04e6dc3d05f4d4a584a8e848a
Aryan Agrawal <79361941+aryanagrawal22@users.noreply.github.com>
2023-11-06 02:50:42 +0530

    feat: added table view for resource page (#300)

--- visualization_tool/src/components/ResourcesList/ResourcesList.tsx
+++ visualization_tool/src/components/ResourcesList/ResourcesList.tsx
@@ -1,7 +1,14 @@
-import {useState} from 'react';
-import {Resource} from '../../types/resources';
-import {typeToImage, statusToColor} from './utils';
-import {useFilter} from './useFilter';
+import { useState } from 'react';
+import { Resource } from '../../types/resources';
+import { typeToImage, statusToColor } from './utils';
+import { useFilter } from './useFilter';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 import Details from './partials/Detalis';
 
 import './ResourcesList.css';
@@ -32,17 +39,36 @@ const ResourcesList = ({
     null
   );
   const [openDetails, setOpenDetails] = useState<boolean>(false);
+
+  const [isGridView, setIsGridView] = useState<boolean>(true);
+
   return (
     <div className="resources-list">
-      <h1>{resources.length > 0 ? 'Found Resources' : 'No Resources Found'}</h1>
+      <div className="header-container">
+        <h1 className='header-text'>{resources.length > 0 ? 'Found Resources' : 'No Resources Found'}</h1>
+        <div className="button-container">
+          <button className={isGridView ? 'active' : ''} onClick={() => { setIsGridView(true) }}>
+            <span>
+              <img src="./icons/grid.png" alt="" className="icon" />
+            </span>
+            <h4>Grid View</h4>
+          </button>
+          <button className={!isGridView ? 'active' : ''} onClick={() => { setIsGridView(false) }}>
+            <span>
+              <img src="./icons/list.png" alt="" className="icon" />
+            </span>
+            <h4>List View</h4>
+          </button>
+        </div>
+      </div>
       <Details
         selectedResource={selectedResource}
         openDetails={openDetails}
         setOpenDetails={setOpenDetails}
       />
-      <div className="resources-list__table">
-        {filteredResources.map(resource => {
-          return (
+      {isGridView ?
+        <div className="resources-list__table">
+          {filteredResources.map((resource) => (
             <div
               className="resources-list__table__card"
               key={resource.id ? resource.id : resource.name}
@@ -69,9 +95,50 @@ const ResourcesList = ({
                 Details
               </button>
             </div>
-          );
-        })}
-      </div>
+          ))}
+        </div>
+        :
+        <TableContainer component={Paper}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell></TableCell>
+                <TableCell>Resource Type</TableCell>
+                <TableCell>Name</TableCell>
+                <TableCell>Status</TableCell>
+                <TableCell>Action</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {filteredResources.map((resource) => (
+                <TableRow key={resource.id ? resource.id : resource.name} >
+                  <TableCell><img src={typeToImage(resource)} className="resources-image" /></TableCell>
+                  <TableCell>{resource.type}</TableCell>
+                  <TableCell>{resource.name}</TableCell>
+                  <TableCell><p
+                    style={{
+                      color: statusToColor(resource.status),
+                    }}
+                  >
+                    {resource.status}
+                  </p></TableCell>
+                  <TableCell className='table-button'>
+                    <button
+                      onClick={() => {
+                        setSelectedResource(resource);
+                        setOpenDetails(true);
+                      }}
+                    >
+                      Details
+                    </button>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      }
+
     </div>
   );
 };
