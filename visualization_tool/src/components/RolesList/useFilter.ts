4520e7e4544f74cc09e8e511103f7aff5cd75e11
Aryan Agrawal <aryanagrawal20023@gmail.com>
2023-11-10 16:25:16 +0530

    feat: added regex for roles page

--- visualization_tool/src/components/RolesList/useFilter.ts
+++ visualization_tool/src/components/RolesList/useFilter.ts
@@ -11,14 +11,27 @@ export const useFilter = (
 
   useEffect(() => {
     const filterRoles = () => {
-      setFilteredRoles(
-        roles.filter(role => {
-          return (
-            role.members.some(member => member.email.includes(email)) &&
-            allowedProjects.includes(role.projectId)
+      let filtered = roles.filter(role => {
+        return allowedProjects.includes(role.projectId);
+      });
+
+      if (email.trim() !== '') {
+        try {
+          const regex = new RegExp(email, 'i');
+          filtered = filtered.filter(role =>
+            role.members.some(member => regex.test(member.email))
           );
-        })
-      );
+        } catch (error) {
+          // If the RegExp constructor throws an error, do a normal search
+          filtered = filtered.filter(role =>
+            role.members.some(member =>
+              member.email.toLowerCase().includes(email.toLowerCase())
+            )
+          );
+        }
+      }
+
+      setFilteredRoles(filtered);
     };
 
     debounce(filterRoles, 200)();
