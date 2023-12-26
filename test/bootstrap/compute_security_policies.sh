a7ac19ddff7e542e91efef6640cb5a035c9ae4fb
Shenoy <shravanshenoy1998@hotmail.com>
2023-12-26 15:35:55 +0530

    add bootstrap script to create security policy and sample output for testing

--- test/bootstrap/compute_security_policies.sh
+++ test/bootstrap/compute_security_policies.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-gcloud services enable compute.googleapis.com
+# gcloud services enable compute.googleapis.com
+
+# create security policy named "test-security-policy"
+gcloud compute security-policies create test-security-policy \
+    --description "policy for external users"
+
+# update default rule to deny all traffic (default rule has priority 2147483647)
+gcloud compute security-policies rules update 2147483647 \
+    --security-policy test-security-policy \
+    --action "deny-404"
